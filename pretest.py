6156560db0fe7de3401a4b110f388f37f5f52dba
Ale <deferrari.alessandro@gmail.com>
2023-06-24 16:49:03 +0200

    new data_retrieval.py

--- pretest.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-
-ds = xr.open_dataset('./resized/test.nc')
-print(ds)
-
-print(ds['CH4_column_volume_mixing_ratio_dry_air'].data.min())
-print(ds['CH4_column_volume_mixing_ratio_dry_air'].data.max())
